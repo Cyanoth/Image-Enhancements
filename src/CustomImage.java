31a5d7a714f89793a4224f5c4d737dae18506417
Cyanoth <Cyanoth@users.noreply.github.com>
2017-10-16 18:37:48 +0100

    Better CustomImage class.
    More modulaised for further operations.

--- src/CustomImage.java
+++ src/CustomImage.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,7 +10,8 @@ import java.io.IOException;
 public class CustomImage {
     private int imgWidth;
     private int imgHeight;
-    private int[][] gsPixel;
+    private int[][] pixelArray;
+    private int[][] modPixelArray;
 
     public CustomImage(String imgPath)
     {
@@ -18,11 +20,12 @@ public class CustomImage {
             img = ImageIO.read(new File(imgPath));
             imgWidth = img.getWidth();
             imgHeight = img.getHeight();
-            gsPixel = new int[imgWidth][imgHeight]; //Create an array which has the space of the image.
+            pixelArray = new int[imgWidth][imgHeight]; //Instantiate two arrays WxH
+            modPixelArray = new int[imgWidth][imgHeight];
 
             for (int x = 0; x < imgWidth; x++)
                 for (int y = 0; y < imgHeight; y++) {
-                    gsPixel[x][y] = img.getData().getSample(x, y, 0);
+                    pixelArray[x][y] = img.getData().getSample(x, y, 0); //Read each pixel as grayscale and store in array.
                 }
 
         }
@@ -34,77 +37,77 @@ public class CustomImage {
     public int getWidth()
     {
         return imgWidth;
-    }
-
+    } //Return the width of the image (px)
     public int getHeight()
     {
         return imgHeight;
+    } //Return the height of the image (px)
+    public int[][] getPixelArray() { return pixelArray; } //Return original, unmodified pixel array for read-only
+    public int[][] getModifiedPixelArray() { return modPixelArray; } //Return modified pixel array
+
+    public int getPixelGrayscaleVal(int x, int y, int[][] pixelArr)
+    {
+        return pixelArr[x][y]; //Get the grayscale value (0-255) in an array from the parameter pixelArr
     }
 
-    public int[][] gsPixels()
+    public int getPixelRGBValue(int x, int y, int[][] pixelArr)
     {
-        return gsPixel;
+        //Since all images for the coursework are grayscale, the value for RGB are all the same.
+        //This simply sets each of the RGB Components to the same value (required for image output)...
+        int rgb =  pixelArr[x][y];
+        rgb = (rgb << 8) + pixelArr[x][y];
+        rgb = (rgb << 8) +  pixelArr[x][y];
+
+        return rgb; //Get the RGB value (0-255) in an array from the parameter pixelArr
     }
 
-    public int getGsPixelValue(int x, int y)
+    public void setModifyPixel(int x, int y, int val)
     {
-        return gsPixel[x][y];
+        if (val > 255)
+            System.out.println("Warning! Modified pixel value is greater than 255! x: " + x + "  y: " + y + " val:" + val);
+
+        modPixelArray[x][y] = val;
     }
 
-    public int getRGBPixelValue(int x, int y)
-    {
-        int rgb =  gsPixel[x][y];
-        rgb = (rgb << 8) +  gsPixel[x][y];
-        rgb = (rgb << 8) +  gsPixel[x][y];
+    public void outputModifiedImage(boolean openFile) {
+        try {
+            File outputFile = new File("C:\\Users\\Charlie\\IdeaProjects\\ImageAnalysisCW\\res\\output.bmp");
+            BufferedImage modImage = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB);
 
-        return rgb;
-    }
-
-    public int getRGBPixelValue(int[][] array, int x, int y)
-    {
-        int rgb =  array[x][y];
-        rgb = (rgb << 8) +  array[x][y];
-        rgb = (rgb << 8) +  array[x][y];
-
-        return rgb;
-    }
-
-    public void outputPixelArray()
-    {
-        int lineCounter = 0;
-
-        for (int y = 0; y < imgHeight; y++)
-            for (int x = 0; x < imgWidth; x++) {
-                if (lineCounter == imgWidth)
-                {
-                    System.out.println(gsPixel[x][y]);
-                    lineCounter = 0;
-                }
-                else
-                {
-                    System.out.print(gsPixel[x][y]  + "  ");
-                    lineCounter++;
+            for (int y = 0; y < imgHeight; y++) {
+                for (int x = 0; x < imgWidth; x++) {
+                    modImage.setRGB(x, y, getPixelRGBValue(x, y, modPixelArray));
                 }
             }
+            ImageIO.write(modImage, "bmp", outputFile);
+
+            if (openFile) {
+                Desktop dt = Desktop.getDesktop();
+                dt.open(outputFile);
+            }
+
+            System.out.println("The image file was written successfully!!");
+
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred during picture export... " + e);
+        }
     }
 
-
-    public void outputPixelArray(int[][] array)
+    public void outToConsole(int[][] array)
     {
         int lineCounter = 0;
 
         for (int y = 0; y < imgHeight; y++)
             for (int x = 0; x < imgWidth; x++) {
-                if (lineCounter == imgWidth)
-                {
-                    System.out.println(array[x][y]);
+                System.out.print(array[x][y]  + "  ");
+
+                if (lineCounter == 10){
+                    System.out.print("\n");
                     lineCounter = 0;
                 }
                 else
-                {
-                    System.out.print(array[x][y]  + "  ");
                     lineCounter++;
-                }
             }
     }
 }
