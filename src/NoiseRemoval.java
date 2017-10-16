9a453a98fd49f5c01c38027e23ec32feb1763f91
Cyanoth <Cyanoth@users.noreply.github.com>
2017-10-16 18:42:07 +0100

    Fix Unweighted Mean Average
    To match the modified CustomImage class.

--- src/NoiseRemoval.java
+++ src/NoiseRemoval.java
@@ -8,40 +8,17 @@ public class NoiseRemoval {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Applying a Low-Pass Filter (Local Neighbourhood with weighted-pixes");
-        CustomImage img = new CustomImage("C:\\Users\\Charlie\\IdeaProjects\\ImageAnalysisCW\\res\\PandaNoise.bmp");
-        ApplyUnweightedMeanAverage(img);
+        CustomImage imageFile = new CustomImage("C:\\Users\\Charlie\\IdeaProjects\\ImageAnalysisCW\\res\\PandaNoise.bmp");
+        ApplyUnweightedMeanAverage(imageFile);
        // img.outputPixelArray();
 
         //testCreateImage(img);
 
     }
 
-    public static void writeImage(CustomImage img, int[][] array) throws IOException {
-        BufferedImage image = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
-
-                int gsValue = img.getRGBPixelValue(array, x, y);
-                image.setRGB(x, y, gsValue);
-            }
-        }
-
-        File outputFile = new File("C:\\Users\\Charlie\\IdeaProjects\\ImageAnalysisCW\\res\\output.bmp");
-        ImageIO.write(image, "bmp", outputFile);
-
-        Desktop dt = Desktop.getDesktop();
-        dt.open(outputFile);
-
-        System.out.println("Image was written successfully!!");
-    }
 
     public static void ApplyUnweightedMeanAverage(CustomImage img) throws IOException {
-        int[][] modifiedImage = new int[img.getWidth()][img.getHeight()];
-
         //iY = Image Y, iX = Image X, bY = Neighbour-Box Y, bX = Neighbour-Box X.
-
         for (int iY = 0; iY < img.getHeight(); iY++) //For each pixel in the Image (Height then Row)
             for (int iX = 0; iX < img.getWidth(); iX++)
             {
@@ -52,16 +29,17 @@ public class NoiseRemoval {
                 for(int bY = Math.max(0, iY - 1); bY < Math.min(img.getHeight(), iY + 2); bY++) //might be iY+1+1
                     for(int bX = Math.max(0, iX -1); bX < Math.min(img.getWidth(), + iX + 2); bX++)
                     {
-                        pixelSum += img.getGsPixelValue(bX, bY);
+                        pixelSum += img.getPixelGrayscaleVal(bX, bY, img.getPixelArray());
                         neighbourCount++;
                     }
 
-                modifiedImage[iX][iY] = Math.round(pixelSum / neighbourCount);
+                img.setModifyPixel(iX, iY, Math.round(pixelSum / neighbourCount));
             }
 
-            img.outputPixelArray(modifiedImage);
-            writeImage(img, modifiedImage);
-            System.out.println("\n\nx: 137, y:76 equal " + modifiedImage[137][76]);
+            img.outToConsole(img.getModifiedPixelArray());
+            img.outputModifiedImage(true);
+
+            System.out.println("\n\nx: 137, y:76 equal " + img.getPixelGrayscaleVal(137, 76, img.getModifiedPixelArray()));
     }
 
 }
